ba8b87de00a67614c856cc466f2ca8a0e3482477
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-14 18:49:35 -0800

    PieChart.tsx imported into home.tsx with title, value, series and colors as props, ReactApexChart imported into PieChart for prop data rendering

--- client/src/components/charts/PieChart.tsx
+++ client/src/components/charts/PieChart.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import ReactApexChart from 'react-apexcharts';
+import { Typography, Box, Stack } from '@pankod/refine-mui';
 
-const PieChart = () => {
+import { PieChartProps } from 'interfaces/home';
+
+const PieChart = ({ title, value, series, colors }: PieChartProps) => {
   return (
-    <div>PieChart</div>
-  )
+    <Box>
+      <Stack direction='column'>
+        <Typography fontSize={14}>
+          {title}
+        </Typography>
+        <Typography fontSize={24} fontWeight={700} mt={1}>
+          {value}
+        </Typography>
+      </Stack>
+      <ReactApexChart 
+        options={{
+          chart: { type: 'donut'},
+          colors,
+          legend: { show: false},
+          dataLabels: { enabled: false},
+        }}
+        series={series}
+        type='donut'
+        width='120px'
+      />
+    </Box>
+    )
 }
 
 export default PieChart
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,4 +1,5 @@
 import { useList } from "@pankod/refine-core/dist/hooks";
+import { Typography, Box, Stack } from "@pankod/refine-mui";
 
 import {
   PieChart,
@@ -11,7 +12,37 @@ import {
 
 const home = () => {
   return (
-    <div>home</div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700}>
+        Dashboard
+      </Typography>
+      <Box mt='20px' display='flex' flexWrap='wrap' gap={4}>
+        <PieChart 
+          title='Properties for Sale'
+          value={684}
+          series={[75, 25]}
+          colors={[ '#475BE8', '#E4E8EF']}
+        />
+        <PieChart 
+          title='Properties for Rent'
+          value={550}
+          series={[60, 40]}
+          colors={[ '#475AE8', '#E4B8EF']}
+        />
+        <PieChart 
+          title='Total Customers'
+          value={5602}
+          series={[75, 25]}
+          colors={[ '#275BE8', '#C4E8EF']}
+        />
+        <PieChart 
+          title='Total for Cities'
+          value={556}
+          series={[75, 25]}
+          colors={[ '#475BE8', '#E4E8EF']}
+        />
+      </Box>
+    </Box>
   )
 }
 
