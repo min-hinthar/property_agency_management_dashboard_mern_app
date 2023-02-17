c2b0616006ea99848a1271025b7f610f3cc53340
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-17 14:07:32 -0800

    pages/all-properties.tsx updated currentFilterValues to return propertyType: from mapped options of dropdown MenuItem and find item with field propertyType from logicalFilters

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -35,6 +35,7 @@ const AllProperties = () => {
 
     return {
       title: logicalFilters.find((item) => item.field === 'title')?.value || '',
+      propertyType: logicalFilters.find((item) => item.field === 'propertyType')?.value || '',
     }
   }, [filters])
 
@@ -47,7 +48,7 @@ const AllProperties = () => {
       <Box mt='20px' sx={{ display: 'flex', flexWrap: 'wrap', gap: 3 }}>
         <Stack direction='column' width='100%'>
           <Typography fontSize={25} fontWeight={700} fontFamily='Manrope'>
-            {!allProperties.length ? 'There are no properties added yet!' : 'All Properties'}
+            {!allProperties.length ? 'No properties with search criteria. Please add a property.' : 'All Properties'}
           </Typography>
           <Box mb={2} mt={3} display='flex' width='84%' justifyContent='space-between' flexWrap='wrap'>
             <Box display='flex' gap={2} flexWrap='wrap' mb={{ xs: '20px', sm: 0 }}>
@@ -81,12 +82,25 @@ const AllProperties = () => {
                   'aria-label' : 'Without label'
                 }}
                 defaultValue=''
-                value=''
-                onChange={() => {}}
+                value={currentFilterValues.propertyType}
+                onChange={(e) => {
+                  setFilters([
+                    {
+                      field: 'propertyType',
+                      operator: 'eq',
+                      value: e.target.value
+                    }
+                  ], 'replace')
+                }}
               >
                 <MenuItem value=''>
                   All
                 </MenuItem>
+                  {['Apartment', 'Villa', 'Farmhouse', 'Condos', 'Townhouse', 'Duplex', 'Studio', 'Chalet'].map((type) => (
+                    <MenuItem key={type} value={type.toLowerCase()}>
+                      {type}
+                    </MenuItem>
+                  ))}
               </Select>
             </Box>
           </Box>
