f151d184c42852c4ecc5fd5edc689d23baf60b8c
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-17 19:48:01 -0800

    controllers/user getAllUsers function updated await User.find({}).limit(req.query._end)

--- client/src/pages/agent.tsx
+++ client/src/pages/agent.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useList } from "@pankod/refine-core";
+import { Box, Typography } from '@pankod/refine-mui';
 
-const agent = () => {
+import { AgentCard } from "components";
+
+const Agents = () => {
+
+  const { data, isLoading, isError } = useList({
+    resource: 'users',
+  });
+
+  const allAgents = data?.data ?? [];
+
+  if(isLoading) return <Typography>Loading All Properties...</Typography>
+  if(isError) return <Typography>Error Loading Properties...</Typography>
+  
   return (
-    <div>agent</div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700}>
+        Agents List
+      </Typography>
+    </Box>
   )
 }
 
-export default agent
+export default Agents
