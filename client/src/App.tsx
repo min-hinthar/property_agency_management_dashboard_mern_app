67bb81b0e5a706bc403267bec9ed256360578c3c
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-18 15:14:10 -0800

    server live on render.com, url updated for client side App.tsx. if(profileObj) fetch POST user, dataProvider url updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -60,7 +60,7 @@ function App() {
       // Save User to MongoDB if Login
 
       if(profileObj) {
-        const response = await fetch('http://localhost:8080/api/v1/users', {
+        const response = await fetch('https://refine-property-management-dashboard.onrender.com/api/v1/users', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
@@ -131,7 +131,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://refine-property-management-dashboard.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
